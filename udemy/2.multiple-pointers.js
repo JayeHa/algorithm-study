57786ff5223803c7edc94290d4ea6f0d392a46be
EllyHa <jwha94@gmail.com>
2022-08-01 16:11:17 +0900

    sliding window

--- udemy/2.multiple-pointers.js
+++ udemy/2.multiple-pointers.js
@@ -76,3 +76,42 @@
   }
   countUniqueValues([1, 2, 2, 5, 7, 7, 99]);
 }
+
+// 📌 SLIDING WINDOW
+// A naive solution
+{
+  function maxSubarraySum(arr, num) {
+    if (num > arr.length) {
+      return null;
+    }
+    var max = -Infinity;
+    for (let i = 0; i < arr.length - num + 1; i++) {
+      temp = 0;
+      for (let j = 0; j < num; j++) {
+        temp += arr[i + j];
+      }
+      if (temp > max) {
+        max = temp;
+      }
+    }
+    return max;
+  }
+}
+
+// Refactor
+{
+  function maxSubarraySum(arr, num) {
+    let maxSum = 0;
+    let tempSum = 0;
+    if (arr.length < num) return null;
+    for (let i = 0; i < num; i++) {
+      maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++) {
+      tempSum = tempSum - arr[i - num] + arr[i];
+      maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+  }
+}
