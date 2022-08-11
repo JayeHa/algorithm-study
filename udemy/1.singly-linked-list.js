df56efedafa7c9bc5e9333a7788d079b462f651d
EllyHa <jwha94@gmail.com>
2022-08-11 10:59:21 +0900

    128. 단일 연결 리스트: reserve 메소드 ✨

--- udemy/1.singly-linked-list.js
+++ udemy/1.singly-linked-list.js
@@ -114,11 +114,38 @@ class SinglyLinkedList {
     this.length--;
     return removed;
   }
+
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+
+    let next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
+
+  print() {
+    const arr = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+    console.log(arr);
+  }
 }
 
 const list = new SinglyLinkedList();
 list.push("HELLO");
 list.push("GOODBYE");
 list.push("!!");
-console.log(list.insert(1, "HELLO WORLD!"));
-console.log(list);
+list.print();
+console.log(list.reverse());
+list.print();
