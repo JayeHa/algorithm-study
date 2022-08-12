97cd73c29efbe5f5618e930ec1b22c546a88d1bd
EllyHa <jwha94@gmail.com>
2022-08-12 12:14:41 +0900

    DLL unshift - Exercise

--- udemy/3.exercise.js
+++ udemy/3.exercise.js
@@ -37,34 +37,56 @@
       return this;
     }
   }
+}
 
-  var doublyLinkedList = new DoublyLinkedList();
-  doublyLinkedList.push(5); // doublyLinkedList
-  doublyLinkedList.length; // 1
-  doublyLinkedList.head.val; // 5
-  doublyLinkedList.tail.val; // 5
-  doublyLinkedList.head.prev; // null
-  doublyLinkedList.push(10);
-  doublyLinkedList;
-  doublyLinkedList.length; // 2
-  doublyLinkedList.head.val; // 5
-  doublyLinkedList.head.next.val; // 10
-  doublyLinkedList.tail.val; // 10
-  doublyLinkedList.head.next.prev.val; // 10
-  doublyLinkedList.push(15);
-  doublyLinkedList;
-  doublyLinkedList.length; // 3
-  doublyLinkedList.head.val; // 5
-  doublyLinkedList.tail.val; // 15
-  doublyLinkedList.tail.prev.val; // 10
-  doublyLinkedList.head.next.next.val; // 15
+/**
+ * DLL unshift - Exercise
+  Implement the following on the DoublyLinkedList class
 
-  // doublyLinkedList.pop().val; // 15
-  // doublyLinkedList.length; // 2
-  // doublyLinkedList.pop().val; // 10
-  // doublyLinkedList.length; // 1
-  // doublyLinkedList.pop().val; // 5
-  // doublyLinkedList.length; // 0
-  // doublyLinkedList.pop(); // undefined
-  // doublyLinkedList.length; // 0
+  unshift
+  This function should accept a value and add a node to the beginning of the DoublyLinkedList with the given value. It should return the DoublyLinkedList.
+ */
+{
+  class Node {
+    constructor(val) {
+      this.val = val;
+      this.next = null;
+      this.prev = null;
+    }
+  }
+
+  class DoublyLinkedList {
+    constructor(val) {
+      this.val = val;
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+    }
+    push(val) {
+      const node = new Node(val);
+      if (this.head === null) {
+        this.head = node;
+        this.tail = this.head;
+      } else {
+        this.tail.next = node;
+        node.prev = this.tail;
+        this.tail = node;
+      }
+      this.length++;
+      return this;
+    }
+    unshift(val) {
+      const newNode = new Node(val);
+      if (!this.head) {
+        this.head = newNode;
+        this.tail = this.head;
+      } else {
+        this.head.prev = newNode;
+        newNode.next = this.head;
+        this.head = newNode;
+      }
+      this.length++;
+      return this;
+    }
+  }
 }
