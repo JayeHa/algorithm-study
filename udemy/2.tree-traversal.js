6eb04e2f660c167c227b2eb0e45d447a88b289a6
EllyHa <jwha94@gmail.com>
2022-08-19 14:12:35 +0900

    178. 깊이 우선 탐색 - 후위 순회

--- udemy/2.tree-traversal.js
+++ udemy/2.tree-traversal.js
@@ -103,6 +103,20 @@ class BinarySearchTree {
 
     return data;
   }
+
+  DFSPostOrder() {
+    const data = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+
+    traverse(this.root);
+
+    return data;
+  }
 }
 
 const tree = new BinarySearchTree();
@@ -113,4 +127,4 @@ tree.insert(15);
 tree.insert(3);
 tree.insert(8);
 tree.insert(20);
-console.log(tree.DFSPreOrder());
+console.log(tree.DFSPostOrder());
